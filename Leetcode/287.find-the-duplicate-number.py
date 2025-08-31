da27ffafdec45046b48da83b1f87dfa4b50c54db
hexa <sushanpoudel80@gmail.com>
2025-08-31 15:31:12 +0545

    add: add solution for 287

---
+++ Leetcode/287.find-the-duplicate-number.py
@@ -0,0 +1,25 @@
+#
+# @lc app=leetcode id=287 lang=python3
+#
+# [287] Find the Duplicate Number
+#
+
+# @lc code=start
+class Solution:
+    def findDuplicate(self, nums: list[int]) -> int:
+        slow = nums[0]
+        fast = nums[0]
+        while True:
+            slow = nums[slow]
+            fast = nums[nums[fast]]
+            if slow == fast:
+                break
+        slow = nums[0]
+        while slow != fast:
+            slow = nums[slow]
+            fast = nums[fast]
+        return slow
+        
+        
+# @lc code=end
+
