0ae0c1f694aa160e70b0a42234843dde17897ebf
hexa01 <sushanpoudel80@gmail.com>
2025-09-14 20:36:40 +0545

    update: update solution for 7

--- Leetcode/7.reverse-integer.py
+++ Leetcode/7.reverse-integer.py
@@ -7,21 +7,27 @@
 # @lc code=start
 class Solution:
     def reverse(self, x: int) -> int:
-        def is_32bit(a, b):
-            return (a * 10 + b <= 2**31 - 1)
+        INT_MAX = 2**31 - 1
+        INT_MIN = -2**31
+
+        def is_32bit(a, b, negative=False):
+            if negative:
+                return a < (-INT_MIN) // 10 or (a == (-INT_MIN) // 10 and b <= (-INT_MIN) % 10)
+            else:
+                return a < INT_MAX // 10 or (a == INT_MAX // 10 and b <= INT_MAX % 10)
+
         dup_x = abs(x)
         reverse = 0
+        negative = x < 0
+
         while dup_x > 0:
             last = dup_x % 10
-            if is_32bit(reverse, last):
-                reverse = reverse * 10 + last
-            else:
+            if not is_32bit(reverse, last, negative):
                 return 0
+            reverse = reverse * 10 + last
             dup_x //= 10
-        if x < 0:
-            return -reverse
-        else:
-            return reverse
+
+        return -reverse if negative else reverse
         
 # @lc code=end
 
